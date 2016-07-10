9709e37620a9bbc38734d253860494df1460dd38
davidlindenbaum <dlindenb@andrew.cmu.edu>
2016-07-09 23:44:45 -0700

    cleanup

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -19,3 +19,19 @@ alias exip='curl ipecho.net/plain ; echo'
 alias runx='run xwin -multiwindow -clipboard'
 
 alias wln='CYGWIN=winsymlinks:nativestrict ln -s'
+
+
+alias ga='git add'
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gd='git diff | vim -'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gcb='git copy-branch-name'
+alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gr='git stash && git svn rebase && git svn dcommit && git stash pop' # git refresh
+alias gv='git --version'
+alias gitedit='e $(git status --porcelain | awk '{print $2}')'
--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,4 @@
 export EDITOR='subl'
-export ANT_HOME='/home/David/bin/apache-ant-1.9.6'
-export PATH=$ANT_HOME/bin:$PATH
 
 if [[ $(uname -o) == "Cygwin" ]]
 then
