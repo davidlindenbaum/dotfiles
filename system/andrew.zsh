45268a7054cb70bec071a25e94c70f076981c17b
David Lindenbaum <dlindenb@andrew.cmu.edu>
2016-07-10 02:35:17 -0400

    remove andrew.zsh

--- system/andrew.zsh
+++
@@ -1,8 +0,0 @@
-if [[ $USER == dlindenb ]]; then
-  export PATH=/afs/cs.cmu.edu/academic/class/15410-f15/bin:$PATH
-  aklog cs.cmu.edu
-  alias simics='env SIMICS_TEXT_CONSOLE="yes" simics46'
-  alias simicss='env SIMICS_TEXT_CONSOLE="yes" simics46serial'
-else
-  #DISPLAY=localhost:0.0 ; export DISPLAY
-fi
