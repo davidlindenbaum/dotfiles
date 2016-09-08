d27c6131848080935f1d91c47912708281be4241
David Lindenbaum <davidslindenbaum@gmail.com>
2016-09-08 18:34:46 -0400

    update submodules

--- zsh/plugins_config.zsh
+++ zsh/plugins_config.zsh
@@ -3,6 +3,6 @@ bindkey '^ ' autosuggest-accept
 
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
-ZSH_HIGHLIGHT_STYLES[comment]='fg=10'
-ZSH_HIGHLIGHT_STYLES[globbing]='none'
-ZSH_HIGHLIGHT_STYLES[path]='none'
+#ZSH_HIGHLIGHT_STYLES[comment]='fg=10'
+#ZSH_HIGHLIGHT_STYLES[globbing]='none'
+#ZSH_HIGHLIGHT_STYLES[path]='none'
