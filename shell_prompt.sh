820b41a5577ba72227374071f33ebc68f802557e
David Lindenbaum <davidslindenbaum@gmail.com>
2016-12-12 19:30:06 -0500

    some more prompt improvements

--- shell_prompt.sh
+++ shell_prompt.sh
@@ -1,8 +1,8 @@
 function __promptline_host {
-  local only_if_ssh="1"
-
-  if [ ! $only_if_ssh -o -n "${SSH_CLIENT}" ]; then
-    if [[ -n ${ZSH_VERSION-} ]]; then print %m; elif [[ -n ${FISH_VERSION-} ]]; then hostname -s; else printf "%s" \\h; fi
+  retval=""
+  if [ -n "${SSH_CLIENT}" ]; then
+    if [[ -n ${ZSH_VERSION-} ]]; then retval="%m";
+    else retval="\\h"; fi
   fi
 }
 function __promptline_cwd {
@@ -16,6 +16,7 @@ function __promptline_cwd {
 
   local cwd="${PWD/#$HOME/$tilde}"
 
+  retval=""
   # get first char of the path, i.e. tilde or slash
   [[ -n ${ZSH_VERSION-} ]] && first_char=$cwd[1,1] || first_char=${cwd::1}
 
@@ -28,168 +29,192 @@ function __promptline_cwd {
     cwd="${cwd%/*}"
 
     formatted_cwd="$dir_sep$part$formatted_cwd"
-    part_count=$((part_count+1))
+    ((part_count++))
 
     [[ $part_count -eq $dir_limit ]] && first_char="$truncation" && break
   done
 
-  printf "%s" "$first_char$formatted_cwd"
-}
-function __promptline_last_exit_code {
-
-  [[ $last_exit_code -gt 0 ]] || return 1;
-
-  printf "%s" "$last_exit_code"
+  retval="$first_char$formatted_cwd"
 }
 function custom_git_branch {
   local branch
   local branch_symbol=" "
+  retval=""
 
   if branch=$( { git symbolic-ref --quiet HEAD || git rev-parse --short HEAD; } 2>/dev/null ); then
     branch=${branch##*/}
-    printf "%s " "${branch_symbol}${branch:-unknown}"
-  local added_symbol="⚫ "
-  local unmerged_symbol="✗ "
-  local modified_symbol="+"
-  local clean_symbol="✔"
-  local has_untracked_files_symbol="…"
+    retval="${branch_symbol}${branch:-unknown} "
+    local added_symbol="⚫ "
+    local unmerged_symbol="✗ "
+    local modified_symbol="+"
+    local clean_symbol="✔"
+    local has_untracked_files_symbol="…"
 
-  local ahead_symbol="↑"
-  local behind_symbol="↓"
+    local ahead_symbol="↑"
+    local behind_symbol="↓"
 
-  local unmerged_count=0 modified_count=0 has_untracked_files=0 added_count=0 is_clean=""
+    local unmerged_count=0 modified_count=0 has_untracked_files=0 added_count=0 is_clean=""
 
-  set -- $(git rev-list --left-right --count @{upstream}...HEAD 2>/dev/null)
-  local behind_count=$1
-  local ahead_count=$2
+    set -- $(git rev-list --left-right --count @{upstream}...HEAD 2>/dev/null)
+    local behind_count=$1
+    local ahead_count=$2
 
-  while read line; do
-    case "$line" in
-      M*) modified_count=$(( $modified_count + 1 )) ;;
-      U*) unmerged_count=$(( $unmerged_count + 1 )) ;;
-    esac
-  done < <(git diff --name-status)
+    added_count=$(git diff --name-status --cached | wc -l)
+    modified_count=$(git diff --name-status --diff-filter=M | wc -l)
+    unmerged_count=$(git diff --name-status --diff-filter=U | wc -l)
 
-  while read line; do
-    case "$line" in
-      *) added_count=$(( $added_count + 1 )) ;;
-    esac
-  done < <(git diff --name-status --cached)
+    if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+      has_untracked_files=1
+    fi
 
-  if [ -n "$(git ls-files --others --exclude-standard)" ]; then
-    has_untracked_files=1
-  fi
+    if [ $(( unmerged_count + modified_count + has_untracked_files + added_count )) -eq 0 ]; then
+      is_clean=1
+    fi
 
-  if [ $(( unmerged_count + modified_count + has_untracked_files + added_count )) -eq 0 ]; then
-    is_clean=1
-  fi
-
-  local leading_whitespace=""
-  [[ $ahead_count -gt 0 ]]         && { printf "%s" "$leading_whitespace$ahead_symbol$ahead_count"; leading_whitespace=" "; }
-  [[ $behind_count -gt 0 ]]        && { printf "%s" "$leading_whitespace$behind_symbol$behind_count"; leading_whitespace=" "; }
-  [[ $modified_count -gt 0 ]]      && { printf "%s" "$leading_whitespace$modified_symbol$modified_count"; leading_whitespace=" "; }
-  [[ $unmerged_count -gt 0 ]]      && { printf "%s" "$leading_whitespace$unmerged_symbol$unmerged_count"; leading_whitespace=" "; }
-  [[ $added_count -gt 0 ]]         && { printf "%s" "$leading_whitespace$added_symbol$added_count"; leading_whitespace=" "; }
-  [[ $has_untracked_files -gt 0 ]] && { printf "%s" "$leading_whitespace$has_untracked_files_symbol"; leading_whitespace=" "; }
-  [[ $is_clean -gt 0 ]]            && { printf "%s" "$leading_whitespace$clean_symbol"; leading_whitespace=" "; }
-    return
-  fi
+    local leading_whitespace=""
+    [[ $ahead_count -gt 0 ]]         && { retval="$retval$leading_whitespace$ahead_symbol$ahead_count"; leading_whitespace=" "; }
+    [[ $behind_count -gt 0 ]]        && { retval="$retval$leading_whitespace$behind_symbol$behind_count"; leading_whitespace=" "; }
+    [[ $modified_count -gt 0 ]]      && { retval="$retval$leading_whitespace$modified_symbol$modified_count"; leading_whitespace=" "; }
+    [[ $unmerged_count -gt 0 ]]      && { retval="$retval$leading_whitespace$unmerged_symbol$unmerged_count"; leading_whitespace=" "; }
+    [[ $added_count -gt 0 ]]         && { retval="$retval$leading_whitespace$added_symbol$added_count"; leading_whitespace=" "; }
+    [[ $has_untracked_files -gt 0 ]] && { retval="$retval$leading_whitespace$has_untracked_files_symbol"; leading_whitespace=" "; }
+    [[ $is_clean -gt 0 ]]            && { retval="$retval$leading_whitespace$clean_symbol"; leading_whitespace=" "; }
+      return
+    fi
   return 1
 }
 function __promptline_ps1 {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
+  tmp_retval=""
 
   # section "a" header
   slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "a" slices
-  __promptline_wrapper "$(__promptline_host)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_host
+  if [[ -n "$retval" ]]; then
+    tmp_retval="$tmp_retval$slice_prefix$retval$slice_suffix"
+    slice_prefix="$slice_joiner"
+    is_prompt_empty=0
+  fi
 
   # section "b" header
   slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "b" slices
-  __promptline_wrapper "$(if [[ -n ${ZSH_VERSION-} ]]; then print %n; elif [[ -n ${FISH_VERSION-} ]]; then printf "%s" "$USER"; else printf "%s" \\u; fi )" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  if [[ -n ${ZSH_VERSION-} ]]; then
+    tmp_retval="$tmp_retval$slice_prefix%n$slice_suffix"
+  else
+    tmp_retval="$tmp_retval$slice_prefix\\u$slice_suffix"
+  fi
+  slice_prefix="$slice_joiner"
+  is_prompt_empty=0
 
   # section "c" header
   slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "c" slices
-  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_cwd
+  if [[ -n "$retval" ]]; then
+    tmp_retval="$tmp_retval$slice_prefix$retval$slice_suffix"
+    slice_prefix="$slice_joiner"
+    is_prompt_empty=0
+  fi
 
   # section "y" header
   #slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_sep}${space}" slice_empty_prefix="${y_fg}${y_bg}${space}"
   #[ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "y" slices
-  #__promptline_wrapper "$(custom_git_branch)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  #custom_get_branch
+  #if [[ -n "$retval" ]]; then
+  #  tmp_retval="$tmp_retval$slice_prefix$retval$slice_suffix"
+  #  slice_prefix="$slice_joiner"
+  #  is_prompt_empty=0
+  #fi
 
   # section "warn" header
+  retval=""
+  if [ "$last_exit_code" -ne "0" ];then
+    retval="${last_exit_code}"
+  fi
   slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_sep}${space}" slice_empty_prefix="${warn_fg}${warn_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "warn" slices
-  __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  if [[ -n "$retval" ]]; then
+    tmp_retval="$tmp_retval$slice_prefix$retval$slice_suffix"
+    slice_prefix="$slice_joiner"
+    is_prompt_empty=0
+  fi
 
   # close sections
-  printf "%s" "${reset_bg}${sep}$reset$space"
-}
-function __promptline_wrapper {
-  # wrap the text in $1 with $2 and $3, only if $1 is not empty
-  # $2 and $3 typically contain non-content-text, like color escape codes and separators
-
-  [[ -n "$1" ]] || return 1
-  printf "%s" "${2}${1}${3}"
+  retval="$tmp_retval${reset_bg}${sep}$reset$space"
 }
 function __promptline_left_prompt {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
+  tmp_retval=""
 
   # section "a" header
   slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "a" slices
-  __promptline_wrapper "$(__promptline_host)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_host
+  if [[ -n "$retval" ]]; then
+    tmp_retval="$tmp_retval$slice_prefix$retval$slice_suffix"
+    slice_prefix="$slice_joiner"
+    is_prompt_empty=0
+  fi
 
   # section "b" header
   slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "b" slices
-  __promptline_wrapper "$(if [[ -n ${ZSH_VERSION-} ]]; then print %n; elif [[ -n ${FISH_VERSION-} ]]; then printf "%s" "$USER"; else printf "%s" \\u; fi )" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  if [[ -n ${ZSH_VERSION-} ]]; then
+    tmp_retval="$tmp_retval$slice_prefix%n$slice_suffix"
+  else
+    tmp_retval="$tmp_retval$slice_prefix\\u$slice_suffix"
+  fi
+  slice_prefix="$slice_joiner"
+  is_prompt_empty=0
 
   # section "c" header
   slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "c" slices
-  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_cwd
+  if [[ -n "$retval" ]]; then
+    tmp_retval="$tmp_retval$slice_prefix$retval$slice_suffix"
+    slice_prefix="$slice_joiner"
+    is_prompt_empty=0
+  fi
 
   # close sections
-  printf "%s" "${reset_bg}${sep}$reset$space"
+  retval="$tmp_retval${reset_bg}${sep}$reset$space"
 }
 function __promptline_right_fast {
-  local slice_prefix slice_empty_prefix slice_joiner slice_suffix
-
-  # section "warn" header
-  slice_prefix="${warn_sep_fg}${rsep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_rsep}${space}" slice_empty_prefix=""
-  # section "warn" slices
-  __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
+  retval=""
+  if [ "$last_exit_code" -ne "0" ]; then
+    retval="${warn_sep_fg}${rsep}${warn_fg}${warn_bg}${space}${last_exit_code}$space${warn_sep_fg}"
+  fi
 }
 function __promptline_right_slow {
-  local slice_prefix slice_empty_prefix slice_joiner slice_suffix
+  tmp_retval=""
 
-  slice_prefix="${y_sep_fg}${rsep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_rsep}${space}" slice_empty_prefix=""
-  # section "y" slices
-  __promptline_wrapper "$(custom_git_branch)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
+  custom_git_branch
+  if [[ -n "$retval" ]]; then
+    tmp_retval="$tmp_retval${y_sep_fg}${rsep}${y_fg}${y_bg}${space}$retval$space${y_sep_fg}"
+  fi
+  retval=$tmp_retval
 }
 function __promptline {
   local last_exit_code="${PROMPTLINE_LAST_EXIT_CODE:-$?}"
 
-  local esc=$'[' end_esc=m
+  local esc=$'['
   if [[ -n ${ZSH_VERSION-} ]]; then
     local noprint='%{' end_noprint='%}'
-  elif [[ -n ${FISH_VERSION-} ]]; then
-    local noprint='' end_noprint=''
   else
     local noprint='\[' end_noprint='\]'
   fi
-  local wrap="$noprint$esc" end_wrap="$end_esc$end_noprint"
+  local wrap="$noprint$esc" end_wrap="m$end_noprint"
   local space=" "
   local sep=""
   local rsep=""
@@ -215,7 +240,8 @@ function __promptline {
 
   function async() {
       # save to temp file
-      printf "%s" "$(__promptline_right_slow)" > "/dev/shm/zsh_prompt_$$"
+      __promptline_right_slow
+      printf "%s" "$retval" > "/dev/shm/zsh_prompt_$$"
       # signal parent
       kill -s USR1 $$
   }
@@ -225,19 +251,29 @@ function __promptline {
         kill -s HUP $ASYNC_PROC >/dev/null 2>&1 || :
     fi
     # start background computation
-    async &!
-    ASYNC_PROC=$!
-    PROMPT="$(__promptline_left_prompt)"
-    RPROMPT="$(__promptline_right_fast)"'$RPROMPTSLOW'"$reset"
+    if [ "$PWD" != "$NOT_GIT_DIR" ]; then
+      async &!
+      ASYNC_PROC=$!
+    else
+      RPROMPTSLOW=""
+    fi
+    __promptline_left_prompt
+    PROMPT="$retval"
+    __promptline_right_fast
+    RPROMPT="$retval"'$RPROMPTSLOW'"$reset"
   else
-    PS1="$(__promptline_ps1)"
+    __promptline_ps1
+    PS1="$retval"
   fi
 }
 
 ASYNC_PROC=0
 function TRAPUSR1() {
     # read from temp file
-    RPROMPTSLOW="$(cat /dev/shm/zsh_prompt_$$)"
+    read RPROMPTSLOW < "/dev/shm/zsh_prompt_$$"
+    if [[ -z $RPROMPTSLOW ]]; then
+      NOT_GIT_DIR=$PWD
+    fi
 
     # reset proc number
     ASYNC_PROC=0
